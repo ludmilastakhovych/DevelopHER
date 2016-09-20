3b8631c0ac09853045a49319a9743739767dea98
Ludmila Stakhovych <ludmila.stakhovych@c02s121rg8wp.myob.myobcorp.net>
2016-09-20 16:50:33 +1000

    palindrome update

--- Palindrome/Palindrome.js
+++ Palindrome/Palindrome.js
@@ -1,35 +1,34 @@
-var word = "racecar"; 
-var isPalindrome = true;
-var startOfWord = 0;
-var endOfWord = word.length - 1;
-var middleOfWord = Math.floor(endOfWord/2);
+//var word = "racecar";
 
-if (word.length%2 === 0) {
-    while (startOfWord <= middleOfWord  && isPalindrome === true) {
-       if (word.charAt(startOfWord) === word.charAt(endOfWord)) {
-            startOfWord++;
-            endOfWord--;
-       }
-       else {
-            isPalindrome = false;
-       }
-    }
-}
-else {
-    while (startOfWord < middleOfWord  && isPalindrome === true) {
-       if (word.charAt(startOfWord) === word.charAt(endOfWord)) {
-            startOfWord++;
-            endOfWord--;
-       }
-       else {
-            isPalindrome = false;
-       }
-    }
-}
+module.exports.isPalindrome = function(word) {
 
-if (isPalindrome === true) {
-    console.log("Word " + word + " is a palindrome!");
-}
-else {
-    console.log("Word " + word + " is NOT a palindrome!");
-}
+    var isPalindrome = true;
+    var startOfWord = 0;
+    var endOfWord = word.length - 1;
+    var middleOfWord = Math.floor(endOfWord/2);
+
+    if (word.length%2 === 0) {
+        while (startOfWord <= middleOfWord  && isPalindrome === true) {
+            if (word.charAt(startOfWord) === word.charAt(endOfWord)) {
+                startOfWord++;
+                endOfWord--;
+            }
+            else {
+                isPalindrome = false;
+            }
+        }
+    }
+    else {
+        while (startOfWord < middleOfWord  && isPalindrome === true) {
+            if (word.charAt(startOfWord) === word.charAt(endOfWord)) {
+                startOfWord++;
+                endOfWord--;
+            }
+            else {
+                isPalindrome = false;
+            }
+        }
+    }
+
+    return isPalindrome;
+};
---
+++ Palindrome/Test/test.js
@@ -0,0 +1,5 @@
+var pal = require('../Palindrome');
+
+var result = pal.isPalindrome("abba");
+
+console.log("it is " + result);
